463e84363644baa2f06b5776aaeb9a68cfecebcb
Erick <erickkan2004@gmail.com>
2024-09-04 20:50:56 -0300

    countdown created

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,28 @@
 gsap.from(".content", 1.2, {opacity: 0, y: 150, delay: 0.5});
+
+let countdownDate = new Date("Sep 30, 2024 00:00:00").getTime();
+
+const interval = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = String(days).padStart(2, '0');
+    document.getElementById("hours").innerHTML = String(hours).padStart(2, '0');
+    document.getElementById("minutes").innerHTML = String(minutes).padStart(2, '0');
+    document.getElementById("seconds").innerHTML = String(seconds).padStart(2, '0');;
+
+    if(distance < 0){
+        clearInterval(interval);
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+        
+    }
+
+}, 1000);
